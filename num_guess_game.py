4513dd64d983f44cd6f3c8db2bff2ed755321e96
Jonathan Perdomo <Jonathan.Perdomo@live.com>
2021-05-02 19:29:53 -0400

    Update num_guess_game.py
    Removed function

--- num_guess_game.py
+++ num_guess_game.py
@@ -1,39 +1,56 @@
 import random
 
+tries = 1
+guess = 0
+high_score = 9999
+play_again = "y"
 
-def start_game():
-
+while play_again == 'y':
     # Print a text header to welcome the player
     print("---------------------------------------------------------")
     print("Welcome to the number guessing game!")
     print("---------------------------------------------------------")
 
-    # create a  random number
-    r_number = random.randint(1,10)
-    tries = 0
+    print(f"The current high score is: {high_score}")
     print("I'm thinking of a number between 1 and 10")
 
-    guess = input("Enter your guess ")
+    # create a  random number
+    r_number = random.randint(1,10)
+
     # Keep asking to guess the number
-    while True:
-        #if the guess is right. end loop and sate the number of tries
-        if int(guess) == r_number:
-            print("Great you got it! It took you", tries, "tries.")
-            break
+    while guess != r_number:
+        guess = input("Enter your guess between 1-10: ")
+        try:
+            int(guess)
+        except:
+            print("Make sure you are entering a number from 1-10")
+            guess = 0
+            tries += 1
+
+        guess = int(guess)
+
+        if guess > 10:
+            print("You're guess is not in the range 1-10. Try again!")
+            tries += 1
         #if the number that was guessed is lower. Ask to take another guess and add 1 to the tries.
-        elif int(guess) < r_number:
-            print("You're guess is lower than my number.")
+        elif guess < r_number and guess > 0:
+            print("You're guess is lower than my number. Try again!")
             tries += 1
-            guess = input("Try again! Enter a new guess ")
         #if the number is not smaller and not correct then it must be higher. Ask to try again
-        else:
-            print("You're guess is higer then my number.")
+        elif guess > r_number:
+            print("You're guess is higher then my number. Try again")
             tries += 1
-            guess = input("Try again! Enter a new guess ")
 
 
+    print(f"You got it! The number was {r_number}. It took you {tries} tries!")
+    if tries < high_score:
+        print("New high score!!")
+        high_score = tries
 
 
-start_game()
+    play_again = input("Enter Y to play again ")
+    play_again = play_again.lower()
+
+
 
 print("Game Over! Thanks For Playing!")
